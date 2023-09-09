b7c1de608ba84a20ad96131995a661efec6ab51b
Zaibonisha <zsmohamed94@gmail.com>
2023-09-10 00:13:34 +0200

    Changes

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,8 +1,8 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true },
-  passwordHash: { type: String, required: true },
+  email: { type: String, required: true }, // Add the email field
+  passwordHash: { type: String, required: true }, // Add the password field
   username: { type: String, required: true }, // Add the username field
 });
 
